c76c57e292042ca39b2e1c0cede04341aba678d7
SNoof85 <snoof85@gmail.com>
2020-01-06 15:02:30 +0100

    Add custom icons

--- tempometer-gauge-card.js
+++ tempometer-gauge-card.js
@@ -103,6 +103,27 @@ class TempometerGaugeCard extends HTMLElement {
             text-align: center;
             margin: 0 auto;
         }
+      .icon1{
+            width: 18px;
+            height: 18px;
+            fill: var(--state-icon-color, currentcolor);
+            float: left;
+            padding-top: 3em;
+        }
+      .icon2{
+            width: 18px;
+            height: 18px;
+            fill: var(--state-icon-color, currentcolor);
+            padding-top: .5em;
+            padding-right: 9px;
+        }
+      .icon3{
+            width: 18px;
+            height: 18px;
+            fill: var(--state-icon-color, currentcolor);
+            float: right;
+            padding-top: 3em;
+        }
       .weathericon-pouring{
             float: left;
             padding-top: 3em;
@@ -191,7 +212,12 @@ class TempometerGaugeCard extends HTMLElement {
       }
     `;
     content.innerHTML = `
-    <div id="gauge-icons" class="gauge-icons">
+    <div id="gauge-icons" class="gauge-icons" style="display: none;">
+        <ha-icon class="icon1" icon="${cardConfig.icon1}"></ha-icon>
+        <ha-icon  class="icon2" icon="${cardConfig.icon2}"></ha-icon>
+        <ha-icon class="icon3" icon="${cardConfig.icon3}"></ha-icon>
+    </div>
+    <div id="gauge-icons-baro" class="gauge-icons">
         <svg class="weathericon-pouring" style="width: 18px;height: 18px;" viewBox="0 0 24 24">
     <path fill="#607D8B" d="M9,12C9.53,12.14 9.85,12.69 9.71,13.22L8.41,18.05C8.27,18.59 7.72,18.9 7.19,18.76C6.65,18.62 6.34,18.07 6.5,17.54L7.78,12.71C7.92,12.17 8.47,11.86 9,12M13,12C13.53,12.14 13.85,12.69 13.71,13.22L11.64,20.95C11.5,21.5 10.95,21.8 10.41,21.66C9.88,21.5 9.56,20.97 9.7,20.43L11.78,12.71C11.92,12.17 12.47,11.86 13,12M17,12C17.53,12.14 17.85,12.69 17.71,13.22L16.41,18.05C16.27,18.59 15.72,18.9 15.19,18.76C14.65,18.62 14.34,18.07 14.5,17.54L15.78,12.71C15.92,12.17 16.47,11.86 17,12M17,10V9A5,5 0 0,0 12,4C9.5,4 7.45,5.82 7.06,8.19C6.73,8.07 6.37,8 6,8A3,3 0 0,0 3,11C3,12.11 3.6,13.08 4.5,13.6V13.59C5,13.87 5.14,14.5 4.87,14.96C4.59,15.43 4,15.6 3.5,15.32V15.33C2,14.47 1,12.85 1,11A5,5 0 0,1 6,6C7,3.65 9.3,2 12,2C15.43,2 18.24,4.66 18.5,8.03L19,8A4,4 0 0,1 23,12C23,13.5 22.2,14.77 21,15.46V15.46C20.5,15.73 19.91,15.57 19.63,15.09C19.36,14.61 19.5,14 20,13.72V13.73C20.6,13.39 21,12.74 21,12A2,2 0 0,0 19,10H17Z"></path>
         </svg>
@@ -260,13 +286,20 @@ class TempometerGaugeCard extends HTMLElement {
     this._config = cardConfig;
 	
 	if (card_style == "thermometer") {
-		root.getElementById("gauge-icons").style.display = 'none';
+		root.getElementById("gauge-icons-baro").style.display = 'none';
 		root.getElementById("gauge-icons-water").style.display = 'none';
 		root.getElementById("gauge-icons-thermo").style.display = 'block';
-	} else if (card_style == "humidity") {
 		root.getElementById("gauge-icons").style.display = 'none';
+	} else if (card_style == "humidity") {
+		root.getElementById("gauge-icons-baro").style.display = 'none';
 		root.getElementById("gauge-icons-water").style.display = 'block';
 		root.getElementById("gauge-icons-thermo").style.display = 'none';
+		root.getElementById("gauge-icons").style.display = 'none';
+	} else if (card_style == "custom") {
+	    root.getElementById("gauge-icons-baro").style.display = 'none';
+		root.getElementById("gauge-icons-water").style.display = 'none';
+		root.getElementById("gauge-icons-thermo").style.display = 'none';
+		root.getElementById("gauge-icons").style.display = 'block';
 	}
   }
 
