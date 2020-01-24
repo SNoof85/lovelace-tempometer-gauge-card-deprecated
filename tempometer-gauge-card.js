f5999f9373b7cb54bf30dc30f564e6c49eab70d3
SNoof85 <snoof85@gmail.com>
2020-01-24 21:23:28 +0100

    More elegant

--- tempometer-gauge-card.js
+++ tempometer-gauge-card.js
@@ -106,57 +106,21 @@ class TempometerGaugeCard extends HTMLElement {
       .icon1{
             width: 18px;
             height: 18px;
-            fill: var(--state-icon-color, currentcolor);
+            fill: #607D8B;
             float: left;
             padding-top: 3em;
         }
       .icon2{
             width: 18px;
             height: 18px;
-            fill: var(--state-icon-color, currentcolor);
+            fill: #607D8B;
             padding-top: .5em;
             padding-right: 9px;
         }
       .icon3{
             width: 18px;
             height: 18px;
-            fill: var(--state-icon-color, currentcolor);
-            float: right;
-            padding-top: 3em;
-        }
-      .weathericon-pouring{
-            float: left;
-            padding-top: 3em;
-        }
-      .weathericon-partly-cloudy{
-            padding-top: .5em;
-            padding-right: 9px;
-        }
-      .weathericon-sunny{
-            float: right;
-            padding-top: 3em;
-        }
-      .thermometer-low{
-            float: left;
-            padding-top: 3em;
-        }
-      .thermometer{
-            padding-top: .5em;
-            padding-right: 9px;
-        }
-      .thermometer-high{
-            float: right;
-            padding-top: 3em;
-        }
-      .water-off{
-            float: left;
-            padding-top: 3em;
-        }
-      .water-percent{
-            padding-top: .5em;
-            padding-right: 9px;
-        }
-      .water{
+            fill: #607D8B;
             float: right;
             padding-top: 3em;
         }
@@ -214,41 +178,23 @@ class TempometerGaugeCard extends HTMLElement {
     content.innerHTML = `
     <div id="gauge-icons" class="gauge-icons" style="display: none;">
         <ha-icon class="icon1" icon="${cardConfig.icon1}"></ha-icon>
-        <ha-icon  class="icon2" icon="${cardConfig.icon2}"></ha-icon>
+        <ha-icon lass="icon2" icon="${cardConfig.icon2}"></ha-icon>
         <ha-icon class="icon3" icon="${cardConfig.icon3}"></ha-icon>
     </div>
     <div id="gauge-icons-baro" class="gauge-icons">
-        <svg class="weathericon-pouring" style="width: 18px;height: 18px;" viewBox="0 0 24 24">
-    <path fill="#607D8B" d="M9,12C9.53,12.14 9.85,12.69 9.71,13.22L8.41,18.05C8.27,18.59 7.72,18.9 7.19,18.76C6.65,18.62 6.34,18.07 6.5,17.54L7.78,12.71C7.92,12.17 8.47,11.86 9,12M13,12C13.53,12.14 13.85,12.69 13.71,13.22L11.64,20.95C11.5,21.5 10.95,21.8 10.41,21.66C9.88,21.5 9.56,20.97 9.7,20.43L11.78,12.71C11.92,12.17 12.47,11.86 13,12M17,12C17.53,12.14 17.85,12.69 17.71,13.22L16.41,18.05C16.27,18.59 15.72,18.9 15.19,18.76C14.65,18.62 14.34,18.07 14.5,17.54L15.78,12.71C15.92,12.17 16.47,11.86 17,12M17,10V9A5,5 0 0,0 12,4C9.5,4 7.45,5.82 7.06,8.19C6.73,8.07 6.37,8 6,8A3,3 0 0,0 3,11C3,12.11 3.6,13.08 4.5,13.6V13.59C5,13.87 5.14,14.5 4.87,14.96C4.59,15.43 4,15.6 3.5,15.32V15.33C2,14.47 1,12.85 1,11A5,5 0 0,1 6,6C7,3.65 9.3,2 12,2C15.43,2 18.24,4.66 18.5,8.03L19,8A4,4 0 0,1 23,12C23,13.5 22.2,14.77 21,15.46V15.46C20.5,15.73 19.91,15.57 19.63,15.09C19.36,14.61 19.5,14 20,13.72V13.73C20.6,13.39 21,12.74 21,12A2,2 0 0,0 19,10H17Z"></path>
-        </svg>
-        <svg class="weathericon-partly-cloudy" style="width: 18px;height: 18px;" viewBox="0 0 24 24">
-    <path fill="#607D8B" d="M12.74,5.47C15.1,6.5 16.35,9.03 15.92,11.46C17.19,12.56 18,14.19 18,16V16.17C18.31,16.06 18.65,16 19,16A3,3 0 0,1 22,19A3,3 0 0,1 19,22H6A4,4 0 0,1 2,18A4,4 0 0,1 6,14H6.27C5,12.45 4.6,10.24 5.5,8.26C6.72,5.5 9.97,4.24 12.74,5.47M11.93,7.3C10.16,6.5 8.09,7.31 7.31,9.07C6.85,10.09 6.93,11.22 7.41,12.13C8.5,10.83 10.16,10 12,10C12.7,10 13.38,10.12 14,10.34C13.94,9.06 13.18,7.86 11.93,7.3M13.55,3.64C13,3.4 12.45,3.23 11.88,3.12L14.37,1.82L15.27,4.71C14.76,4.29 14.19,3.93 13.55,3.64M6.09,4.44C5.6,4.79 5.17,5.19 4.8,5.63L4.91,2.82L7.87,3.5C7.25,3.71 6.65,4.03 6.09,4.44M18,9.71C17.91,9.12 17.78,8.55 17.59,8L19.97,9.5L17.92,11.73C18.03,11.08 18.05,10.4 18,9.71M3.04,11.3C3.11,11.9 3.24,12.47 3.43,13L1.06,11.5L3.1,9.28C3,9.93 2.97,10.61 3.04,11.3M19,18H16V16A4,4 0 0,0 12,12A4,4 0 0,0 8,16H6A2,2 0 0,0 4,18A2,2 0 0,0 6,20H19A1,1 0 0,0 20,19A1,1 0 0,0 19,18Z"></path>
-        </svg>
-        <svg class="weathericon-sunny" style="width: 18px;height: 18px;" viewBox="0 0 24 24">
-    <path fill="#607D8B" d="M12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,2L14.39,5.42C13.65,5.15 12.84,5 12,5C11.16,5 10.35,5.15 9.61,5.42L12,2M3.34,7L7.5,6.65C6.9,7.16 6.36,7.78 5.94,8.5C5.5,9.24 5.25,10 5.11,10.79L3.34,7M3.36,17L5.12,13.23C5.26,14 5.53,14.78 5.95,15.5C6.37,16.24 6.91,16.86 7.5,17.37L3.36,17M20.65,7L18.88,10.79C18.74,10 18.47,9.23 18.05,8.5C17.63,7.78 17.1,7.15 16.5,6.64L20.65,7M20.64,17L16.5,17.36C17.09,16.85 17.62,16.22 18.04,15.5C18.46,14.77 18.73,14 18.87,13.21L20.64,17M12,22L9.59,18.56C10.33,18.83 11.14,19 12,19C12.82,19 13.63,18.83 14.37,18.56L12,22Z"></path>
-        </svg>
+        <ha-icon class="icon1" icon=mdi:weather-pouring></ha-icon>
+        <ha-icon class="icon2" icon=mdi:weather-partly-cloudy></ha-icon>
+        <ha-icon class="icon3" icon=mdi:weather-sunny></ha-icon>
     </div>
     <div id="gauge-icons-thermo" class="gauge-icons" style="display: none;">
-        <svg class="thermometer-low" style="width: 18px;height: 18px;" viewBox="0 0 24 24">
-    <path fill="#607D8B" d="M15 13V5A3 3 0 0 0 9 5V13A5 5 0 1 0 15 13M12 4A1 1 0 0 1 13 5V12H11V5A1 1 0 0 1 12 4Z"></path>
-        </svg>
-        <svg class="thermometer" style="width: 18px;height: 18px;" viewBox="0 0 24 24">
-    <path fill="#607D8B" d="M15 13V5A3 3 0 0 0 9 5V13A5 5 0 1 0 15 13M12 4A1 1 0 0 1 13 5V8H11V5A1 1 0 0 1 12 4Z"></path>
-        </svg>
-        <svg class="thermometer-high" style="width: 18px;height: 18px;" viewBox="0 0 24 24">
-    <path fill="#607D8B" d="M15 13V5A3 3 0 0 0 9 5V13A5 5 0 1 0 15 13M12 4A1 1 0 0 1 13 5H11A1 1 0 0 1 12 4Z"></path>
-        </svg>
+        <ha-icon class="icon1" icon=mdi:thermometer-low></ha-icon>
+        <ha-icon class="icon2" icon=mdi:thermometer></ha-icon>
+        <ha-icon class="icon3" icon=mdi:thermometer-high></ha-icon>
     </div>
     <div id="gauge-icons-water" class="gauge-icons" style="display: none;">
-        <svg class="water-off" style="width: 18px;height: 18px;" viewBox="0 0 24 24">
-    <path fill="#607D8B" d="M17.12,17.12L12.5,12.5L5.27,5.27L4,6.55L7.32,9.87C6.55,11.32 6,12.79 6,14A6,6 0 0,0 12,20C13.5,20 14.9,19.43 15.96,18.5L18.59,21.13L19.86,19.86L17.12,17.12M18,14C18,10 12,3.2 12,3.2C12,3.2 10.67,4.71 9.27,6.72L17.86,15.31C17.95,14.89 18,14.45 18,14Z"></path>
-        </svg>
-        <svg class="water-percent" style="width: 18px;height: 18px;" viewBox="0 0 24 24">
-    <path fill="#607D8B" d="M12,3.25C12,3.25 6,10 6,14C6,17.32 8.69,20 12,20A6,6 0 0,0 18,14C18,10 12,3.25 12,3.25M14.47,9.97L15.53,11.03L9.53,17.03L8.47,15.97M9.75,10A1.25,1.25 0 0,1 11,11.25A1.25,1.25 0 0,1 9.75,12.5A1.25,1.25 0 0,1 8.5,11.25A1.25,1.25 0 0,1 9.75,10M14.25,14.5A1.25,1.25 0 0,1 15.5,15.75A1.25,1.25 0 0,1 14.25,17A1.25,1.25 0 0,1 13,15.75A1.25,1.25 0 0,1 14.25,14.5Z"></path>
-        </svg>
-        <svg class="water" style="width: 18px;height: 18px;" viewBox="0 0 24 24">
-    <path fill="#607D8B" d="M12,20A6,6 0 0,1 6,14C6,10 12,3.25 12,3.25C12,3.25 18,10 18,14A6,6 0 0,1 12,20Z"></path>
-        </svg>
+        <ha-icon class="icon1" icon=mdi:water-off></ha-icon>
+        <ha-icon class="icon2" icon=mdi:water-percent></ha-icon>
+        <ha-icon class="icon3" icon=mdi:water></ha-icon>
     </div>
     <div class="container">
         <div class="gauge-a">
